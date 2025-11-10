267f3d357cd2d051baf9518514e034122b208ec1
yomiyusuf <ylinkz@gmail.com>
2025-11-11 07:01:13 +1100

    Fix duplication bug

---
+++ core/domain/src/main/java/com/yomi/next2go/core/domain/model/CategoryId.kt
@@ -0,0 +1,14 @@
+package com.yomi.next2go.core.domain.model
+
+enum class CategoryId(val id: String) {
+    GREYHOUND("9daef0d7-bf3c-4f50-921d-8e818c60fe61"),
+    HARNESS("161d9be2-e909-4326-8c2c-35ed71fb460b"),
+    HORSE("4a2788f8-e825-4d36-9894-efd4baf1cfae"),
+    ;
+
+    companion object {
+        fun fromId(id: String): CategoryId? {
+            return entries.find { it.id == id }
+        }
+    }
+}
